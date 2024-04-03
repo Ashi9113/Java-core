2b3e62df70b46a7f5c18412c7b6e93ba62f17750
Ashi9113 <97699720+Ashi9113@users.noreply.github.com>
2024-04-03 16:48:10 +0530

    Add files via upload

---
+++ D01P01.java
@@ -0,0 +1,47 @@
+package com.learning.core.day1session1;
+
+class Book {
+    private String bookTitle;
+    private double bookPrice;
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public double getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(double bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+}
+
+public class D01P01 {
+    public static Book createBooks(String title, double price) {
+        Book book = new Book();
+        book.setBookTitle(title);
+        book.setBookPrice(price);
+        return book;
+    }
+
+    public static void showBooks(Book book) {
+        System.out.println("Book Title: " + book.getBookTitle() + " and Price: " + book.getBookPrice());
+    }
+
+    public static void main(String[] args) {
+        // Input
+        String title = "Advanced Java";
+        double price = 350.00;
+
+        // Creating a book object
+        Book book1 = createBooks(title, price);
+
+        // Showing the book details
+        showBooks(book1);
+    }
+}
